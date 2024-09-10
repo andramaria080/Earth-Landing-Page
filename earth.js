1ffe35cb9577a8430487a47a0f8e4fadfa2cb589
Andra Maria Ene <andramaria080@gmail.com>
2024-09-10 14:17:17 +0100

    Landing page in progress

---
+++ earth.js
@@ -0,0 +1,39 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+const serviceData = document.getElementById("service");
+
+menuBtn.addEventListener("click", function () {
+  navLinks.classList.toggle("active");
+});
+
+const data = [
+  {
+    image: "./img/service-1.png",
+    title: "Web Design",
+    paragraph:
+      "Custom web design for small businesses we help you capture new audience and increase your sales",
+  },
+  {
+    image: "./img/service-2.png",
+    title: "Graphic Design",
+    paragraph:
+      "Logos, merchandise and more. Anyone can create nice graphics. We think it`s better to crate memorable ones. ",
+  },
+  {
+    image: "./img/service-3.png",
+    title: "Content Creation",
+    paragraph:
+      "Want to atract people to your website? You have to have the best content in the world. That`s what we do.",
+  },
+];
+data.forEach((service) => {
+  const serviceElement = `
+  <div class="service-content">
+  <img src="${service.image}" alt="${service.title}"/>
+  <div class="service-data">
+  <h1>${service.title}</h1>
+  <p>${service.paragraph}</p> 
+  </div>
+  </div>`;
+  serviceData.innerHTML += serviceElement;
+});
